374f2dd91a6347a4e13aa2525fb6ac89e2153c30
kossme <Suzape76!>
2020-11-14 15:11:22 +0300

    1

--- src/main/java/rest/service/UserService.java
+++ src/main/java/rest/service/UserService.java
@@ -97,10 +97,6 @@ public class UserService implements UserDetailsService {
     }
 
     public void updateUser(User user) {
-        System.out.println(user);
-        System.out.println(user.getRoles());
-        Set<Role> roles = user.getRoles();
-        System.out.println(roles);
         userRepository.save(user);
     }
 
